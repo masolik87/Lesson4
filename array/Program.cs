9226b324a8a45053d476e558941bda2954d40ee6
Дмитрий <dimon20.07@mail.ru>
2022-09-13 16:51:01 +0300

    array

---
+++ array/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] GetBinaryArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+int[] resultArray = GetBinaryArray(8);
+
+Console.WriteLine($"Result array: [ {String.Join("; ", resultArray)} ]");
